6803ec84b6786d878ccfea971c861709c3884d0c
kamal-atef <marwan.salem209@gmail.com>
2025-02-09 05:07:48 +0200

    animtion finished 1/??

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,22 @@
-import Input from './input'
+'use client'
 import Image from 'next/image'
 import pic from './../../public/pic.jpg'
 import helwan_pic from './../../public/helwan-slider1.jpg'
+import { useEffect, useState } from 'react'
 export default function Home() {
+  useEffect(() => {
+    const observer = new IntersectionObserver((e) => {
+      e.forEach((e) => {
+        console.log(e)
+        if (e.isIntersecting){
+          e.target.classList.add('opacity-100','blur-none','translate-x-none')
+          e.target.classList.remove('blur-sm','translate-x-full')
+        }
+      })
+    })
+    const elements = document.querySelectorAll(".opacity-0");
+    elements.forEach((e) => observer.observe(e))
+  }, []); 
   return (
     <>
       <Image
@@ -10,7 +24,7 @@ export default function Home() {
         src={pic}
         alt="pic of me"
         className="aspect-square col-span-12 sm:col-start-2 sm:col-span-4
-        md:w-90 w-full rounded-full "
+        md:w-90 w-full rounded-full"
       />
       <div className="mt-20 md:mt-0 sm:col-span-4 sm:col-start-8 col-span-11 col-start-2">
         <p className="text-3xl">Hi, I'm Marwan Mohamed</p>
@@ -23,6 +37,7 @@ export default function Home() {
         </p>
       </div>
       <Image
+        priority={true}
         src={helwan_pic}
         alt="helwan university"
         className="aspect-square col-span-12 sm:col-start-8 sm:col-span-4
@@ -30,21 +45,21 @@ export default function Home() {
       />
       <div className="mt-30 md:mt-20 sm:col-span-5 sm:col-start-2 col-span-11 col-start-2">
         <p className="text-2xl">
-          i'm a fullstack web developer with a frontend foucs
+          i'm a frontend web developer who can work fullstack
         </p>
         <p className="text-2xl">i'm proficent in react,Next,Tailwind</p>
         <p className="text-2xl">for backend i use Node or Django</p>
       </div>
       <div
-        className="col-span-12 sm:col-start-8 sm:col-span-4 
-        md:w-20 w-20 mt-5 sm:mt-0 grid-cols-subgrid grid "
+        className="col-span-12 opacity-0 sm:col-start-8 sm:col-span-4 
+        md:w-20 w-20 mt-5 sm:mt-30 grid-cols-subgrid grid transition-all duration-1000 blur-sm translate-x-full"
       >
         <img
           className="col-span-4 col-start-2 sm:col-span-1"
           src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg"
         />
         <img
-          className="col-span-4  col-start-8 sm:col-span-1"
+          className="col-span-4  col-start-8 sm:col-span-1 sm:col-start-3"
           src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/tailwindcss/tailwindcss-original-wordmark.svg"
         />
         <img
@@ -52,7 +67,7 @@ export default function Home() {
           src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nextjs/nextjs-line.svg"
         />
         <img
-          className="col-span-4  col-start-8 sm:col-span-1 sm:col-start-2"
+          className="col-span-4  col-start-8 sm:col-span-1 sm:col-start-3"
           src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nodejs/nodejs-original-wordmark.svg"
         />
       </div>
